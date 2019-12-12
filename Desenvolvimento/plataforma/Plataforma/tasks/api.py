47ef1341e1303bf90c249efedf47bdf8686050d9
fernandesjm <a76331@alunos.uminho.pt>
2019-12-12 13:50:54 +0000

    Adicionadas as bids

--- Desenvolvimento/plataforma/Plataforma/tasks/api.py
+++ Desenvolvimento/plataforma/Plataforma/tasks/api.py
@@ -1,6 +1,6 @@
-from tasks.models import Task 
+from tasks.models import Task, Bid
 from rest_framework import viewsets, permissions
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer,BidSerializer
 from rest_framework import generics, permissions
 from django.core.exceptions import ValidationError
 
@@ -18,4 +18,28 @@ class TaskViewSet(viewsets.ModelViewSet):
 		if self.request.user.is_proponent:
 			serializer.save(owner=self.request.user)
 		else: 
+			raise ValidationError("Não é permitido")
+
+
+class BidViewSet(viewsets.ModelViewSet):
+	permission_classes = [permissions.IsAuthenticated,]
+	serializer_class = BidSerializer
+
+	def get_queryset(self):
+		t = Task.objects.get(id=self.request.query_params['task'])
+		bids = t.bids.all()
+
+		return bids
+
+	def perform_create(self,serializer):
+		id_task = self.request.data['task']
+		t = Task.objects.get(id=id_task)
+
+		if self.request.user.is_bidder and t.is_active:
+			for i in self.request.user.bids.all():
+				if(i.task_id == id_task):
+					raise ValidationError("Não é permitido")
+			
+			serializer.save(bidder=self.request.user)
+		else:
 			raise ValidationError("Não é permitido")
--- Desenvolvimento/plataforma/Plataforma/tasks/urls.py
+++ Desenvolvimento/plataforma/Plataforma/tasks/urls.py
@@ -1,9 +1,9 @@
 from rest_framework import routers
-from .api import TaskViewSet #RegisterTaskAPI
+from .api import TaskViewSet ,BidViewSet#RegisterTaskAPI
 
 router=routers.DefaultRouter()
 router.register('api/tasks', TaskViewSet, 'tasks')
-#router.register('api/register/tasks', RegisterTaskAPI, 'regtasks')
+router.register('api/registar/bid', BidViewSet, 'bids')
 
 
 
@@ -12,6 +12,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', include(router.urls)),
+    #path('api/registar/bid', BidViewSet)
    # path('api/tasks/register', RegisterTaskAPI.as_view(),name='reg_task')
 ]
 
