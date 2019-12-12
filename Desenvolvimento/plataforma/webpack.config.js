4c437ec21cafb83bb3b460e8321aa29f8e990597
tiagofgon <tiago.f@outlook.com>
2019-12-12 17:29:35 +0000

    novo webpack

--- Desenvolvimento/plataforma/webpack.config.js
+++ Desenvolvimento/plataforma/webpack.config.js
@@ -11,6 +11,21 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.(png|svg|pdf|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "url-loader"
+            // options: {
+            //   name: '[path][name]-[hash:8].[ext]'
+            // }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
       }
     ]
   }
