9fe1b0384180c4f5897cd02e375ad8ac095f2fa6
tiagofgon <tiago.f@outlook.com>
2019-12-12 17:21:59 +0000

    novos ficheiros

--- Desenvolvimento/plataforma/Plataforma/tasks/api.py
+++ Desenvolvimento/plataforma/Plataforma/tasks/api.py
@@ -1,45 +1,57 @@
 from tasks.models import Task, Bid
 from rest_framework import viewsets, permissions
-from .serializers import TaskSerializer,BidSerializer
+from .serializers import TaskSerializer, BidSerializer
 from rest_framework import generics, permissions
 from django.core.exceptions import ValidationError
 
+
 class TaskViewSet(viewsets.ModelViewSet):
-	permission_classes = [permissions.IsAuthenticated,]
-	serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = TaskSerializer
 
-	def get_queryset(self):
-		if self.request.user.is_proponent:
-			return self.request.user.tasks.all()
-		else:
-			return Task.objects.all()
+    def get_queryset(self):
+        if self.request.user.is_proponent:
+            return self.request.user.tasks.all()
+        else:
+            return Task.objects.all()
 
-	def perform_create(self,serializer):
-		if self.request.user.is_proponent:
-			serializer.save(owner=self.request.user)
-		else: 
-			raise ValidationError("Não é permitido")
+    def perform_create(self, serializer):
+        if self.request.user.is_proponent:
+            serializer.save(owner=self.request.user)
+        else:
+            raise ValidationError("Não é permitido")
 
 
 class BidViewSet(viewsets.ModelViewSet):
-	permission_classes = [permissions.IsAuthenticated,]
-	serializer_class = BidSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = BidSerializer
 
-	def get_queryset(self):
-		t = Task.objects.get(id=self.request.query_params['task'])
-		bids = t.bids.all()
+    def get_queryset(self):
+        t = Task.objects.get(id=self.request.query_params['task'])
+        bids = t.bids.all()
 
-		return bids
+        return bids
 
-	def perform_create(self,serializer):
-		id_task = self.request.data['task']
-		t = Task.objects.get(id=id_task)
+    def perform_create(self, serializer):
+        id_task = self.request.data['task']
+        t = Task.objects.get(id=id_task)
 
-		if self.request.user.is_bidder and t.is_active:
-			for i in self.request.user.bids.all():
-				if(i.task_id == id_task):
-					raise ValidationError("Não é permitido")
-			
-			serializer.save(bidder=self.request.user)
-		else:
-			raise ValidationError("Não é permitido")
+        if self.request.user.is_bidder and t.is_active:
+            for i in self.request.user.bids.all():
+                if(i.task_id == id_task):
+                    raise ValidationError("Não é permitido")
+
+            serializer.save(bidder=self.request.user)
+        else:
+            raise ValidationError("Não é permitido")
+
+# mostra as bids a todas as tarefas de um bidder
+
+
+class BidderAPI(viewsets.ModelViewSet):
+    permissions_classes = [permissions.IsAuthenticated, ]
+    serializer_class = BidSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_bidder:
+            return self.request.user.bids.all()
--- Desenvolvimento/plataforma/Plataforma/tasks/urls.py
+++ Desenvolvimento/plataforma/Plataforma/tasks/urls.py
@@ -1,28 +1,22 @@
+from django.urls import path, include
 from rest_framework import routers
-from .api import TaskViewSet ,BidViewSet#RegisterTaskAPI
+from .api import TaskViewSet, BidViewSet, BidderAPI  # RegisterTaskAPI
 
-router=routers.DefaultRouter()
+router = routers.DefaultRouter()
 router.register('api/tasks', TaskViewSet, 'tasks')
 router.register('api/registar/bid', BidViewSet, 'bids')
-
-
-
-from django.urls import path,include
+router.register('api/bidder/bids', BidderAPI, 'bids')
 
 
 urlpatterns = [
     path('', include(router.urls)),
     #path('api/registar/bid', BidViewSet)
-   # path('api/tasks/register', RegisterTaskAPI.as_view(),name='reg_task')
+    # path('api/tasks/register', RegisterTaskAPI.as_view(),name='reg_task')
 ]
 
-#urlpatterns = [
+# urlpatterns = [
 #	path('api/tasks', TaskViewSet.as_view()),
 #	path('api/tasks/register', TaskViewSet.as_view()),
 
 
-
-
-
-
-#]
+# ]
