74f9d334a4ef63b0d1bdbf24622202f41ee79856
fernandesjm <a76331@alunos.uminho.pt>
2019-12-30 18:48:15 +0000

    delete das tarefas adicionado

--- Desenvolvimento/plataforma/Plataforma/tasks/api.py
+++ Desenvolvimento/plataforma/Plataforma/tasks/api.py
@@ -21,12 +21,20 @@ class TaskViewSet(viewsets.ModelViewSet):
         else:
             raise ValidationError("Não é permitido")
 
+    def destroy(self,request,*args,**kwargs):
+        if self.request.user.id == self.get_object().owner_id:
+            o=self.get_object()
+            self.perform_destroy(o)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return ValidationError('Não autorizado')
+
 
 class BidViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = BidSerializer
 
-    def get_queryset(self):
+    def get_queryset(self,id):
         t = Task.objects.get(id=self.request.query_params['task'])
         bids = t.bids.all()
 
